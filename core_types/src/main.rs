4722019db0618e489266e9e1fd3e1ff03b0bafe4
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2024-02-01 17:08:33 -0300

    hello world and core types

---
+++ core_types/src/main.rs
@@ -0,0 +1,17 @@
+#[allow(dead_code)]
+#[allow(unused_variables)]
+
+use std::mem;
+
+fn main() {
+    let a: u8 = 123;
+    println!("a = {}", a);
+
+    let mut b: i8 = 0;
+    println!("b = {} before", b);
+    b = 42;
+    println!("b = {} after", b);
+
+    let c = 123456789;
+    println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
+}
