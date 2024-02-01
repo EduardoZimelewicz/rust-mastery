bb20f06e317cfb9d7ae740808ce59402e767afcd
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2024-02-01 17:20:06 -0300

    hello world and core types

--- core_types/src/main.rs
+++ core_types/src/main.rs
@@ -2,6 +2,7 @@
 #[allow(unused_variables)]
 
 use std::mem;
+use std::mem::size_of_val;
 
 fn main() {
     let a: u8 = 123;
@@ -16,4 +17,17 @@ fn main() {
     println!("c = {}, takes up {} bytes", c, mem::size_of_val(&c));
     c = -1;
     println!("c = {}", c);
+
+    let z: isize = 123;
+    let size_of_z = mem::size_of_val(&z);
+    println!("z = {}, takes up {} bytes, {}-bit OS", z, size_of_z, size_of_z*8);
+
+    let d: char = 'x';
+    println!("{} is a char, size = {} bytes", d, size_of_val(&d));
+
+    let e: f32 = 2.5;
+    println!("{}, size = {} bytes", e, size_of_val(&e));
+
+    let g: bool = false;
+    println!("{}, size = {} bytes", g, size_of_val(&g));
 }
