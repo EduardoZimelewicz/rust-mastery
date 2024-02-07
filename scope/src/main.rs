9c7f081f23120b7da989a7d993a3ee6964d73c4c
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2024-02-07 13:42:26 -0300

    scope

---
+++ scope/src/main.rs
@@ -0,0 +1,18 @@
+fn scope_and_shadowing() {
+    let a = 123;
+
+    {
+        let b = 456;
+        println!("inside, b = {}", b);
+
+        let a = 777;
+        println!("inside, a = {}", a);
+    }
+
+    println!("outside, a = {}", a);
+
+}
+
+fn main() {
+    scope_and_shadowing()
+}
